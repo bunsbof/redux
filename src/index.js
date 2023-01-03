ef90959be9ddd6d34a0b9cc2d4cd7f69651d1396
bunsbof <nguyenminhcuong2000@gmail.com>
2023-01-03 15:30:10 +0700

    #2023/01/03 cuong - testing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import { userListSlice } from "./features/Users";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const store = configureStore({
-  reducer: {
-    userList: userListSlice.reducer,
-  },
-});
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
